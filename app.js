a29a53ac87e5fdc41196f87b49434819ca062001
Nelly Mahecha <nejumava@gmail.com>
2020-01-29 14:40:21 -0500

    Solucion a reto

---
+++ app.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
+mongoose.connection.on("error", function(e) { console.error(e); });
+
+// definimos el schema
+var schema = mongoose.Schema({
+    date: { type: Date, default: Date.now },
+    name: String
+});
+  
+// definimos el modelo
+var Visitor = mongoose.model("Visitor", schema);
+
+app.get('/', (req, res) => {
+    var nombre = req.params.name;
+    if(nombre == null || nombre == ''){
+        nombre = "Anónimo";
+    }
+
+    Visitor.create({ name: nombre }, function(err){
+        if (err) return console.error(err);
+        res.send('<h1>El visitante fue almacenado con éxito</h1>');
+    });    
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
